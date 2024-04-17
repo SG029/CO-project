869350da66eb06c53282638dc260bccb817803b5
Sushant Gola <98545506+SG029@users.noreply.github.com>
2024-04-17 23:40:05 +0530

    Add files via upload

--- Simulator.py
+++ Simulator.py
@@ -2,64 +2,40 @@ import time
 
 reg = [['zero', '0x0001000', 0], ['ra', '0x0001001', 0], ['sp', '0x0001002', 256], ['gp', '0x0001003', 0], ['tp', '0x0001004', 0], ['t0', '0x0001005', 0], ['t1', '0x0001006', 0], ['t2', '0x0001007', 0], ['s0', '0x0001008', 0], ['s1', '0x0001009', 0], ['a0', '0x000100a', 0], ['a1', '0x000100b', 0], ['a2', '0x000100c', 0], ['a3', '0x000100d', 0], ['a4', '0x000100e', 0], ['a5', '0x000100f', 0], ['a6', '0x0001010', 0], ['a7', '0x0001011', 0], ['s2', '0x0001012', 0], ['s3', '0x0001013', 0], ['s4', '0x0001014', 0], ['s5', '0x0001015', 0], ['s6', '0x0001016', 0], ['s7', '0x0001017', 0], ['s8', '0x0001018', 0], ['s9', '0x0001019', 0], ['s10', '0x000101a', 0], ['s11', '0x000101b', 0], ['t3', '0x000101c', 0], ['t4', '0x000101d', 0], ['t5', '0x000101e', 0], ['t6', '0x000101f', 0]]
 memory={
-    "0x00010000":"0b00000000000000000000000000000000",
-    "0x00010004":"0b00000000000000000000000000000000",
-    "0x00010008":"0b00000000000000000000000000000000",
-    "0x0001000c":"0b00000000000000000000000000000000",
-    "0x00010010":"0b00000000000000000000000000000000",
-    "0x00010014":"0b00000000000000000000000000000000",
-    "0x00010018":"0b00000000000000000000000000000000",
-    "0x0001001c":"0b00000000000000000000000000000000",
-    "0x00010020":"0b00000000000000000000000000000000",
-    "0x00010024":"0b00000000000000000000000000000000",
-    "0x00010028":"0b00000000000000000000000000000000",
-    "0x0001002c":"0b00000000000000000000000000000000",
-    "0x00010030":"0b00000000000000000000000000000000",
-    "0x00010034":"0b00000000000000000000000000000000",
-    "0x00010038":"0b00000000000000000000000000000000",
-    "0x0001003c":"0b00000000000000000000000000000000",
-    "0x00010040":"0b00000000000000000000000000000000",
-    "0x00010044":"0b00000000000000000000000000000000",
-    "0x00010048":"0b00000000000000000000000000000000",
-    "0x0001004c":"0b00000000000000000000000000000000",
-    "0x00010050":"0b00000000000000000000000000000000",
-    "0x00010054":"0b00000000000000000000000000000000",
-    "0x00010058":"0b00000000000000000000000000000000",
-    "0x0001005c":"0b00000000000000000000000000000000",
-    "0x00010060":"0b00000000000000000000000000000000",
-    "0x00010064":"0b00000000000000000000000000000000",
-    "0x00010068":"0b00000000000000000000000000000000",
-    "0x0001006c":"0b00000000000000000000000000000000",
-    "0x00010070":"0b00000000000000000000000000000000",
-    "0x00010074":"0b00000000000000000000000000000000",
-    "0x00010078":"0b00000000000000000000000000000000",
-    "0x0001007c":"0b00000000000000000000000000000000",  
+    "0x00010000":"0b00000000000000000000000000000000","0x00010004":"0b00000000000000000000000000000000","0x00010008":"0b00000000000000000000000000000000","0x0001000c":"0b00000000000000000000000000000000",
+    "0x00010010":"0b00000000000000000000000000000000","0x00010014":"0b00000000000000000000000000000000","0x00010018":"0b00000000000000000000000000000000","0x0001001c":"0b00000000000000000000000000000000",
+    "0x00010020":"0b00000000000000000000000000000000","0x00010024":"0b00000000000000000000000000000000","0x00010028":"0b00000000000000000000000000000000","0x0001002c":"0b00000000000000000000000000000000",
+    "0x00010030":"0b00000000000000000000000000000000","0x00010034":"0b00000000000000000000000000000000","0x00010038":"0b00000000000000000000000000000000","0x0001003c":"0b00000000000000000000000000000000",
+    "0x00010040":"0b00000000000000000000000000000000","0x00010044":"0b00000000000000000000000000000000","0x00010048":"0b00000000000000000000000000000000","0x0001004c":"0b00000000000000000000000000000000",
+    "0x00010050":"0b00000000000000000000000000000000","0x00010054":"0b00000000000000000000000000000000","0x00010058":"0b00000000000000000000000000000000","0x0001005c":"0b00000000000000000000000000000000",
+    "0x00010060":"0b00000000000000000000000000000000","0x00010064":"0b00000000000000000000000000000000","0x00010068":"0b00000000000000000000000000000000","0x0001006c":"0b00000000000000000000000000000000",
+    "0x00010070":"0b00000000000000000000000000000000","0x00010074":"0b00000000000000000000000000000000","0x00010078":"0b00000000000000000000000000000000","0x0001007c":"0b00000000000000000000000000000000",  
 }
 prog_ln = 1
 
-def dec_2_bin(dec, num_bits = 32, bit_tp = "signed"):
-    if bit_tp == "unsigned":
-        bina = bin(dec)[2:].zfill(num_bits)
+def dec_2_bin(dec, num_bits=32, bit_tp="signed"):
+    if bit_tp=="unsigned":
+        bina=bin(dec)[2:].zfill(num_bits)
         return bina
-    if bit_tp in ("signed", "1s"):
-        if dec >= 0:
-            bina = dec_2_bin(dec, num_bits, "unsigned")
+    if bit_tp in ("signed","1s"):
+        if dec>=0:
+            bina=dec_2_bin(dec, num_bits, "unsigned")
         else:
-            bina = dec_2_bin(abs(dec), num_bits, "unsigned")
-            bina = bina.replace("0", "2")
-            bina = bina.replace("1", "0")
-            bina = bina.replace("2", "1") 
+            bina=dec_2_bin(abs(dec), num_bits, "unsigned")
+            bina=bina.replace("0", "2")
+            bina=bina.replace("1", "0")
+            bina=bina.replace("2", "1") 
         return bina
-    if dec >= 0:
-        bina = bin(dec)[2:].zfill(num_bits)
+    if dec>=0:
+        bina=bin(dec)[2:].zfill(num_bits)
     else:
-        pos_bina = bin(abs(dec))[2:].zfill(num_bits)
-        pos_bina = pos_bina.replace("0","2")
-        pos_bina = pos_bina.replace("1","0")
-        pos_bina = pos_bina.replace("2","1")
-        inv_bina = pos_bina
-        invval = bina_to_dec(inv_bina, "unsigned")
-        bina = dec_2_bin(invval + 1, num_bits, "unsigned")
+        pos_bina=bin(abs(dec))[2:].zfill(num_bits)
+        pos_bina=pos_bina.replace("0","2")
+        pos_bina=pos_bina.replace("1","0")
+        pos_bina=pos_bina.replace("2","1")
+        inv_bina=pos_bina
+        invval=bina_to_dec(inv_bina, "unsigned")
+        bina=dec_2_bin(invval + 1, num_bits, "unsigned")
     return bina
 
 def prin(reg):
@@ -67,30 +43,30 @@ def prin(reg):
         print(i[0],":",i[2])
 
 def bina_to_dec(bina, bit_tp = "signed"):
-    sum = 0
-    if bit_tp == "unsigned":
+    sum=0
+    if bit_tp=="unsigned":
         for i in reversed(range(len(bina))):
-            if bina[i] == "1":
-                sum += 2**(len(bina)-1-i)
+            if bina[i]=="1":
+                sum +=2**(len(bina)-1-i)
         return sum
-    elif bit_tp in ("signed", "1s"):
-        if bina[0] == "1":
-            bina = bina.replace("0", "2")
-            bina = bina.replace("1", "0")
-            bina = bina.replace("2", "1")
+    elif bit_tp in ("signed","1s"):
+        if bina[0]=="1":
+            bina=bina.replace("0", "2")
+            bina=bina.replace("1", "0")
+            bina=bina.replace("2", "1")
             return -bina_to_dec(bina, "unsigned")
         else:
             return bina_to_dec(bina, "unsigned")
-    elif bit_tp == "2s":
-        if bina[0] == "1":
-            bina = bina.replace("0", "2")
-            bina = bina.replace("1", "0")
-            bina = bina.replace("2", "1")
-            return -(bina_to_dec(bina, "unsigned") + 1)
+    elif bit_tp=="2s":
+        if bina[0]=="1":
+            bina=bina.replace("0", "2")
+            bina=bina.replace("1", "0")
+            bina=bina.replace("2", "1")
+            return -(bina_to_dec(bina,"unsigned") + 1)
         else:
-            return bina_to_dec(bina, "unsigned")
+            return bina_to_dec(bina,"unsigned")
     else:
-        return bina_to_dec(bina, "signed")
+        return bina_to_dec(bina,"signed")
 def check_inst(inst_line):
     if (inst_line=="00000000000000000000000001100011"):
         return ["halt","bonus"]
@@ -131,10 +107,10 @@ def check_inst(inst_line):
 
 
 def output(reg):
-    bina_representations = []
+    bina_representations=[]
     for value in reg:
-        if value[2] < 0:bina_value = bin(value[2] & 0xFFFFFFFF)[2:] 
-        else:bina_value = bin(value[2])[2:].zfill(32)
+        if value[2]<0:bina_value = bin(value[2] & 0xFFFFFFFF)[2:] 
+        else:bina_value=bin(value[2])[2:].zfill(32)
         bina_representations.append('0b' + bina_value +" ")
     return ''.join(bina_representations)
 
@@ -142,10 +118,10 @@ def detectregister(bina):return int(bina, 2)
 
 def dec_to_two_unsigned_to_dec(dec):
     num_bits=32
-    if dec >= 0:bina = bin(dec)[2:].zfill(num_bits)
+    if dec>=0:bina=bin(dec)[2:].zfill(num_bits)
     else:
         pos_bina = bin(abs(dec))[2:].zfill(num_bits)
-        inv_bina = ''.join('1' if bit == '0' else '0' for bit in pos_bina)
+        inv_bina = ''.join('1' if bit =='0' else '0' for bit in pos_bina)
         bina = bin(int(inv_bina, 2) + 1)[2:].zfill(num_bits)
     deci = 0
     deci = int(bina, 2)
@@ -341,9 +317,9 @@ def inp1():
             bonus(inst_line[i],check_insta[0],reg)
             if check_insta[0]=='rst':pass
             elif check_insta[0]=='halt':
-                print("0b" + dec_2_bin(prog_ln*4), end = " ")
-                reg[0][2] = 0
-                outdata += "0b" + dec_2_bin(prog_ln*4) + " " + output(reg) + "\n"
+                print("0b"+dec_2_bin(prog_ln*4), end =" ")
+                reg[0][2]=0
+                outdata += "0b"+dec_2_bin(prog_ln*4) + " " + output(reg) +"\n"
                 print(output(reg))
                 break
         elif check_insta[1]=='r':r_Type(inst_line[i],check_insta[0],reg)
@@ -352,9 +328,9 @@ def inp1():
         elif check_insta[1]=='b':b_Type(inst_line[i],check_insta[0],reg)
         elif check_insta[1]=='u':u_Type(inst_line[i],check_insta[0],reg)
         elif check_insta[1]=='j':j_Type(inst_line[i],check_insta[0],reg)
-        print("0b" + dec_2_bin(prog_ln*4), end = " ")
-        reg[0][2] = 0
-        outdata += "0b" + dec_2_bin(prog_ln*4) + " " + output(reg) + "\n"
+        print("0b"+dec_2_bin(prog_ln*4),end=" ")
+        reg[0][2]=0
+        outdata += "0b"+dec_2_bin(prog_ln*4)+" "+ output(reg)+"\n"
         prin(reg)
     with open(dir_path, "w") as f:
         f.write(outdata)
